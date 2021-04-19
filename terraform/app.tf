d9026e94f900af96500cb9ec57c5c17e606c13e4
ravudaiappan <wwwraj@gmail.com>
2021-04-18 23:41:12 -0700

    Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -5,7 +5,7 @@ provider "aws" {
 variable "team-name" {
   description = "team-name"
   type        = string
-  default     = "default-team-name"
+  default     = "lovely-temple"
 }
 
 resource "aws_instance" "web" {
@@ -26,7 +26,7 @@ terraform {
    backend "s3" {
  # Replace this with your bucket name!
    bucket         = "ot-infra-state"
-   key            = "global/s3/terraform.tfstate"
+   key            = "global/s3/terraform.tfstate.lovely-temple"
    region         = "us-east-1"
    }
 }
